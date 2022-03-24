026d0679e8493115b936625294246e41045880c5
IasminiVoltoliniCorso <86911103+IasminiVoltoliniCorso@users.noreply.github.com>
2022-03-23 21:34:41 -0300

    Missão Semana 7

--- Missao Semana 7/MissaoSemana7/src/main/java/MissaoSemana7/Main.java
+++ Missao Semana 7/MissaoSemana7/src/main/java/MissaoSemana7/Main.java
@@ -3,24 +3,27 @@ package MissaoSemana7;
 public class Main {
 
     public static void main(String[] args) {
-        
-        Empregado empregado1 = new Empregado("Iasmini", "Corso", 1000);
-        
-        System.out.println("Nome: " + empregado1.getPrimeiroNome() + " " + empregado1.getSobrenome());
-        
-        System.out.println("Salario anual = " + empregado1.getSalarioAnual());
-        
-        empregado1.aumentarSalario(10);
-        
-        System.out.println("Aumento de 10% = " + empregado1.getSalarioAnual());
-        
-        Empregado empregado2 = new Empregado("Larissa", "Voltolini", 2000);
-        
-        System.out.println("Salario anual = " + empregado2.getSalarioAnual());
-        
-        empregado2.aumentarSalario(10);
-        
-        System.out.println("Aumento de 10% = " + empregado2.getSalarioAnual());
-    }       
-}
 
+        Empregado empregado1 = new Empregado("Iasmini", "Corso", 3000);
+
+        System.out.println("Nome: " + empregado1.getPrimeiroNome() + " " + empregado1.getSobrenome());
+
+        System.out.println("Salario anual = R$ " + empregado1.getSalarioAnual());
+
+        empregado1.aumentarSalario(10);
+
+        System.out.println("Aumento de 10% = R$ " + empregado1.getSalarioAnual());
+
+        System.out.println("-----------------------------------");
+
+        Empregado empregado2 = new Empregado("Larissa", "Voltolini", 4000);
+
+        System.out.println("Nome: " + empregado2.getPrimeiroNome() + " " + empregado2.getSobrenome());
+
+        System.out.println("Salario anual = R$ " + empregado2.getSalarioAnual());
+
+        empregado2.aumentarSalario(10);
+
+        System.out.println("Aumento de 10% = R$ " + empregado2.getSalarioAnual());
+    }
+}
